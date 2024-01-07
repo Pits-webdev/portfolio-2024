fa052ca309bd2500685f73540c815e5e17d42b57
Pits-webdev <pits.webdev@gmail.com>
2024-01-07 16:56:37 +0100

    clear all

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,7 @@
-import About from "@/components/About";
-import Hero from "@/components/Hero";
-
 export default function Home() {
   return (
     <main className='w-full h-full'>
-      <Hero />
-      <About />
+      <p>hi</p>
     </main>
   );
 }
